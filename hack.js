9e49e8523f0702d3e95d9e217500b6c445e7eb6f
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2018-09-19 18:27:08 +0100

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,27 +1,30 @@
 // ROVER656's Javascript Minesweeper Solver
 
-//Find first non-bomb so we can leave it for the player to click, this fixes win issues
-var first = 0;
-for (var i = 0; i < total; i++) {
- if (adjacent[i] != mine)
+function cheatGame(uncheckNonMines)
+{
+ //Find first non-bomb so we can leave it for the player to click, this fixes win issues
+ var first = 0;
+ for (var i = 0; i < total; i++) {
+  if (adjacent[i] != mine)
  {
-  first = i;
-  break;
+   first = i;
+   break;
+  }
  }
-}
 
-//Loop over, flagging all bombs and unhiding all but 1 empty square
-for (var i = 0; i < total; i++) {
- if (adjacent[i] == mine)
- {
-  exposed[i] = flagged;
-  setSq(i);
-  flags++;
-  setMines();
- }
- else if (i != first) {
-  exposed[i] = exposed;
-  setSq(i);
-  remaining--;
+ //Loop over, flagging all bombs and unhiding all but 1 empty square
+ for (var i = 0; i < total; i++) {
+  if (adjacent[i] == mine)
+  {
+   exposed[i] = flagged;
+   setSq(i);
+   flags++;
+   setMines();
+  }
+  else if (i != first && uncheckNonMines) {
+   exposed[i] = exposed;
+   setSq(i);
+   remaining--;
+  }
  }
 }
